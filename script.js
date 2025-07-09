ab0559fc3ebd2d6f883b6cba852b34a64c8ec03e
nweston2 <104042720+nweston2@users.noreply.github.com>
2025-07-09 17:52:35 -0600

    finish front end. start back end

---
+++ script.js
@@ -0,0 +1,6 @@
+function calculate() {
+    const cost = document.getElementById("projectedCost")
+    const hours = document.getElementById("projectedHours")
+    const payment = document.getElementById("halfFirst")
+    const branch = document.getElementById("branch")
+}
